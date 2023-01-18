1b285f4d0469c91c3ee2a055bd6ea4de190cd98b
Christopher Tubbs <ctubbsii@apache.org>
2023-01-17 21:22:33 -0500

    Update and rename src/test/java/org/apache/accumulo/proxy/SelectHalfSelector.java to src/test/java/org/apache/accumulo/proxy/its/SelectHalfSelector.java
    Fix jar sealing error

--- src/test/java/org/apache/accumulo/proxy/its/SelectHalfSelector.java
+++ src/test/java/org/apache/accumulo/proxy/its/SelectHalfSelector.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.accumulo.proxy;
+package org.apache.accumulo.proxy.its;
 
 import java.util.List;
 import java.util.stream.Collectors;
